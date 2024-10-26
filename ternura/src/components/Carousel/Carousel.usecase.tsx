a2a2865126c2ae3ec2ff2268b3e20bc01674743c
Robocotik <alexander.starkin@gmail.com>
2024-10-26 18:59:29 +0300

    make carousel work

--- ternura/src/components/Carousel/Carousel.usecase.tsx
+++
@@ -1,4 +0,0 @@
-export const picturesSrc = [
-  'https://i.pinimg.com/736x/06/00/de/0600dea38233c6938921e472a8e5e2a5.jpg',
-  'https://i.pinimg.com/originals/9f/3b/5e/9f3b5e036232110f3c5f5ada50d653f8.jpg',
-];
