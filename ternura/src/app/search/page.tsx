3fe11e8a672bd4d626ab37e7603603a4e337966f
Robocotik <alexander.starkin@gmail.com>
2024-10-26 17:13:27 +0300

    carousel added

--- ternura/src/app/search/page.tsx
+++ ternura/src/app/search/page.tsx
@@ -1,11 +1,14 @@
 'use client'
+import Card from '@/components/Card';
 import { MainHeader } from '@/components/MainHeader';
+import { CardDefault } from '@/components/Card/Card.usecase';
 
 export default function SearchPage() {  
 
   return (
     <div className='w-full h-full'>
       <MainHeader />
+      <Card {...CardDefault} />
     </div>
   );
 }
--- ternura/src/components/Carousel/Carousel.usecase.tsx
+++ ternura/src/components/Carousel/Carousel.usecase.tsx
@@ -1,5 +1,4 @@
 export const picturesSrc = [
-  'https://avatars.mds.yandex.net/i?id=ff9edb8004e0fa9d283e5fe60cd742fce20eae6d-5386023-images-thumbs&n=13',
   'https://i.pinimg.com/736x/06/00/de/0600dea38233c6938921e472a8e5e2a5.jpg',
   'https://i.pinimg.com/originals/9f/3b/5e/9f3b5e036232110f3c5f5ada50d653f8.jpg',
 ];
