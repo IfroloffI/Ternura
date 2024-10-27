6ab942a58ee874c81cea9843cc863c640928e1bd
Zephrys_TheGreatest <froloff1830@gmail.com>
2024-10-27 13:54:46 +0300

    [ADD] | add new logic of neuro

--- Profile-servce/internal/profileAnalysis/profilesSort.go
+++ Profile-servce/internal/profileAnalysis/profilesSort.go
@@ -6,28 +6,43 @@ import (
 	"github.com/Benzogang-Tape/Ternura/Profile-servce/internal/domain"
 )
 
-type SimilarityScore struct {
-	Profile domain.UserProfile
-	Score   float64
-}
-
 func SortProfilesForSimilarity(targetProfile domain.UserProfile, profiles []domain.UserProfile) ([]domain.UserProfile, error) {
 	var similarityScores []SimilarityScore
 
 	for _, profile := range profiles {
-		score := calculateSimilarity(profile, targetProfile)
+		score := CalculateSimilarity(profile, targetProfile)
 		similarityScores = append(similarityScores, SimilarityScore{Profile: profile, Score: score})
 	}
 
-	// Сортировка по возрастанию (меньше значение — больше схожесть)
 	sort.Slice(similarityScores, func(i, j int) bool {
 		return similarityScores[i].Score < similarityScores[j].Score
 	})
 
-	// Возвращаем отсортированный список анкет
 	sortedProfiles := []domain.UserProfile{}
 	for _, item := range similarityScores {
 		sortedProfiles = append(sortedProfiles, item.Profile)
 	}
 	return sortedProfiles, nil
+}
+
+// Новая
+func RecommendProfiles(user domain.UserProfile) []SimilarityScore {
+	var similarityScores []SimilarityScore
+	var dataset // TODO передать dataset
+	for _, otherUser := range dataset {
+		if otherUser.UserID == user.UserID {
+			continue // Пропускаем самого себя
+		}
+		similarity := CalculateSimilarity(user, otherUser)
+		if similarity > 0.0 { // Мы можем включить профили с любым рейтинговым значением
+			similarityScores = append(similarityScores, SimilarityScore{Profile: otherUser, Score: similarity})
+		}
+	}
+
+	// Сортируем по рейтингу сходства
+	sort.Slice(similarityScores, func(i, j int) bool {
+		return similarityScores[i].Score > similarityScores[j].Score
+	})
+
+	return similarityScores
 }
