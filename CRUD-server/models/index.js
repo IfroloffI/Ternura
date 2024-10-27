f06c88624bb3ee7eeabbf9c18b68732d3cf2dda4
Zephrys_TheGreatest <froloff1830@gmail.com>
2024-10-27 18:39:07 +0300

    [UPDATE] | rename server

--- CRUD-server/models/index.js
+++ CRUD-server/models/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const Profiles = require("../models");
+const Profiles = require(".");
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/myapp',);
